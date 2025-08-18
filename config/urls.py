95cc0cf7f8a4cac42f934b9a31954608708e23c2
a1ry1337 <mixail-aristov07072003@mail.ru>
2025-08-19 01:16:23 +0500

    init

--- config/urls.py
+++ config/urls.py
@@ -5,11 +5,6 @@ from apps.auth_user.views import router as auth_router
 
 api = NinjaAPI()
 api.add_router("/auth/", auth_router)
-
-@api.get("/hello")
-def hello(request):
-    return {"message": "Hello from Djan1go Ninja"}
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", api.urls),
